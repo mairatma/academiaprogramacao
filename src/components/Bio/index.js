a38a256c0fcb101e5a41cff69b353d1f3ef10089
Maira Bello <maira.araujo@liferay.cloud>
2021-02-08 12:24:45 -0300

    Add real text

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -1,4 +1,4 @@
-import { Avatar, Card, Image } from 'antd';
+import { Avatar } from 'antd';
 import React from 'react'
 
 import pedroPhoto from './images/pedro.jpg';
@@ -8,12 +8,15 @@ import './bio.css';
 const Bio = () => {
   return (
     <div className="pb-bio">
-      <Card>
-        <div className="pb-bio__card">
-          <Avatar alt="Pedro Bello" src={pedroPhoto} />
-          <Card.Meta title="Pedro Bello" description="@pedrobello59" />
-        </div>
-      </Card>
+      <Avatar alt="Pedro Bello" src={pedroPhoto} />
+      <div className="pb-bio__text">
+        <span className="pb-bio__name">Pedro Bello</span> é formado em Engenharia da Computação pelo CIn-UFPE. Durante a graduação,
+        participou de competições de programação, sendo duas vezes campeão nacional da Maratona de Programação da SBC
+        (<a href="http://maratona.sbc.org.br/hist/2008/index.html">2008</a> e <a href="http://maratona.sbc.org.br/hist/2009/index.html">2009</a>)
+        e uma vez campeão latino-americano no International Collegiate Programming Contest (2010). Após se formar, Pedro trabalhou
+        por um ano como engenheiro de software na Google. Entre 2013 e 2020 trabalhou na In Loco, onde liderou o desenvolvimento da
+        tecnologia de localização da empresa. Em 2015, concluiu o Mestrado em Ciência da Computação no CIn-UFPE.
+      </div>
     </div>
   )
 }
--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -13,7 +13,12 @@ function MainContent() {
       <Banner />
       <div className="pb-main__sections">
         <div className="pb-main__description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci porta non pulvinar neque laoreet suspendisse interdum. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci porta non pulvinar neque laoreet suspendisse interdum. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci porta non pulvinar neque laoreet suspendisse interdum. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci porta non pulvinar neque laoreet suspendisse interdum. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel.
+        <p>Com a Academia de Programação quero criar uma comunidade (física e virtual) dedicada ao estudo e à prática da programação. Estou convencido de que os benefícios deste estudo ainda não são plenamente reconhecidos e de que o ensino da programação será cada vez mais difundido nas escolas. Minha ideia é apenas acelerar este processo que já está em curso.</p>
+
+        <p>O primeiro e mais óbvio benefício do estudo da programação é o financeiro. A indústria de software vem engolindo vários nichos do mercado global e tudo indica que ela continuará próspera pelas próximas décadas.</p>
+
+        <p>Há também um benefício que não se restringe ao âmbito profissional, mas deriva do próprio processo de aprendizado da programação: aprender a programar é aprender a disciplinar o pensamento. Um programa feito às pressas ou sem atenção conterá erros (bugs) e não funcionará da forma esperada. Erros de raciocínio lógico que o programador cometer também serão revelados pela execução do programa. Programar é exercitar a mente, e por isso chamei o empreendimento de academia, ao invés de escola ou instituto.</p>
+
         </div>
         <Divider />
         <Bio />
